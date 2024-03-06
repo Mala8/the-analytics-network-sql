8bb1cfb3ccd117b7f1cc4ad2748e688487f7a0d0
Mala8 <156968541+Mala8@users.noreply.github.com>
2024-03-05 23:27:47 -0300

    Update respuestas-1.sql

--- parte-1/homework/respuestas-1.sql
+++ parte-1/homework/respuestas-1.sql
@@ -127,17 +127,44 @@ group by
 	date,
 	store_id
 order by
-	date
+	date;
 -- 9. Obtener las ventas netas y el porcentaje de descuento otorgado por producto en Argentina.
-
+select
+	product,
+	round(sum(sale)-sum(promotion),0) as NetSale,
+	round((sum(promotion) / sum(sale))*100,2) as PerDisscount
+from stg.order_line_sale ols
+left join stg.store_master sm
+on ols.store = sm.store_id
+where sm.country = 'Argentina'
+group by
+	product;
 -- 10. Las tablas "market_count" y "super_store_count" representan dos sistemas distintos que usa la empresa para contar la cantidad de gente que ingresa a tienda, uno para las tiendas de Latinoamerica y otro para Europa. Obtener en una unica tabla, las entradas a tienda de ambos sistemas.
-
+--Ver cambio de formaro Fecha
 -- 11. Cuales son los productos disponibles para la venta (activos) de la marca Phillips?
-
+select *
+from stg.product_master
+where name like '%PHILIPS%'
+and is_active = 'true';
 -- 12. Obtener el monto vendido por tienda y moneda y ordenarlo de mayor a menor por valor nominal de las ventas (sin importar la moneda).
-
+select 
+	store,
+	round(sum(sale),0) sale,
+	currency
+from stg.order_line_sale
+group by 
+	store,
+	currency
+order by sale desc;
 -- 13. Cual es el precio promedio de venta de cada producto en las distintas monedas? Recorda que los valores de venta, impuesto, descuentos y creditos es por el total de la linea.
-
+select
+	ols.product,
+	ols.currency,
+	avg(sale+coalesce(tax,0)-coalesce(promotion,0)-coalesce(credit,0)/quantity) as AVGPrice
+from stg.order_line_sale ols
+group by
+	ols.product,
+	ols.currency;
 -- 14. Cual es la tasa de impuestos que se pago por cada orden de venta?
 
 
