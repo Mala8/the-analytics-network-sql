5bc946663185f8b92e99f2880d1d13d3a80a1e9a
Mala8 <156968541+Mala8@users.noreply.github.com>
2025-05-15 23:00:27 -0300

    Update project-2-answers.sql

--- parte-2/project/project-2-answers.sql
+++ parte-2/project/project-2-answers.sql
@@ -43,5 +43,74 @@ order by
 	sk.item_id
 )
 
+
+cte_sales_usd as (
+
+select 
+	ols.*,
+	pm.*,
+	sm.country as store_country,
+	sm.province as store_province,
+	sm.name as store_name,
+	sp.name as supplier_name,
+	d.day,
+	d.month,
+	d.month_label,
+	d.year,
+	d.fiscal_year,
+	d.fiscal_quarter_label,
+	case
+		when currency = 'ARS' then (coalesce(sale,0) / fx_rate_usd_peso)
+		when currency = 'EUR' then (coalesce(sale,0) / fx_rate_usd_eur) 
+		when currency = 'URU' then (coalesce(sale,0) / fx_rate_usd_uru)
+		else sale 
+	end gross_sale_USD,
+	case
+		when currency = 'ARS' then (coalesce(promotion,0) / fx_rate_usd_peso)
+		when currency = 'EUR' then (coalesce(promotion,0) / fx_rate_usd_eur)
+		when currency = 'URU' then (coalesce(promotion,0) / fx_rate_usd_uru)
+		else promotion 
+	end promotion_USD,
+	case
+		when currency = 'ARS' then (coalesce(credit,0) / fx_rate_usd_peso)
+		when currency = 'EUR' then (coalesce(credit,0) / fx_rate_usd_eur) 
+		when currency = 'URU' then (coalesce(credit,0) / fx_rate_usd_uru)
+		else credit 
+	end credit_USD,
+	case
+		when currency = 'ARS' then (coalesce(tax,0) / fx_rate_usd_peso)
+		when currency = 'EUR' then (coalesce(tax,0) / fx_rate_usd_eur) 
+		when currency = 'URU' then (coalesce(tax,0) / fx_rate_usd_uru)
+		else tax
+	end tax_USD, -- continuar 
+	
+	
+	
+-- joins para one big table y tomar sacar todas las métricas
+from stg.order_line_sale ols
+left join stg.product_master pm
+on ols.product = pm.product_code
+left join stg.cost cs
+on ols.product = cs.product_code
+left join stg.store_master sm
+on ols.store = sm.store_id
+left join stg.monthly_average_fx_rate fx
+on date_trunc('month', ols.date) = fx.month
+left join stg.supplier sp
+on ols.product = sp.product_id
+left join stg.shrinkage sk
+on (extract(year from ols.date) = sk.year)
+and ols.store = sk.store_id
+and ols.product = sk.item_id
+left join stg.return_movements rm
+on ols.order_number = rm.order_id
+and ols.product = rm.item
+and rm.movement_id = 2
+left join stg.date d
+on ols.date = d.date
+where sp.is_primary = true
+)
+
+
 /*Select * 
 from cte_shrinkage*/
